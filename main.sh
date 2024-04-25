56c9a80fc10cb9cb7988be15a2619d77bedc4915
raksbi <rakibur.rahman@sbisecsol.com>
2024-04-25 12:43:00 +0900

    main.sh: outputs as json object instead

--- main.sh
+++ main.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2001
 
-set -euo pipefail
-
 usage() { 
   echo "Usage:" && echo "  $(basename "$0") COMMIT_MSG KEYWORDS (Space seperated string)"; 
   echo "  ENV: [BUILD_COMMIT_DETECTOR_STRICTNESS: default low, can also be moderate or high (ACTION PROVIDED)], [RUNNER_DEBUG: default false (GITHUB PROVIDED)]"
@@ -11,6 +9,18 @@ usage() {
 log_info() { echo "$@" | sed 's/^/INFO:\t/' >&2; }
 log_err() { echo "$@" | sed 's/^/ERROR:\t/' >&2; }
 log_warning() { echo "$@" | sed 's/^/WARNING:\t/' >&2; }
+
+build_necessary_json_output() {
+  local key=$1
+  local value=$2
+  local output
+
+  output="$(jq -n \
+    --arg key "$key" --arg value "$value" '{ build_necessary: { ($key): $value } }'
+  )"
+
+  echo "$output"
+}
 
 detect_build_necessity() {
   local commit_msg=$1
@@ -33,7 +43,9 @@ detect_build_necessity() {
 
   for keyword in "${keywords[@]}"; do
     if $grep -wP "(?<![\w-])$keyword(?![\w-])" <<< "$commit_msg"; then
-      echo "build_necessary=false" | tee -a "${GITHUB_ENV:-/dev/null}" "${GITHUB_OUTPUT:-/dev/null}"
+
+      build_necessary_json_output build false | tee -a "${GITHUB_ENV:-/dev/null}" "${GITHUB_OUTPUT:-/dev/null}"
+
       case ${BUILD_COMMIT_DETECTOR_STRICTNESS:-low} in
         low) return 0 ;;
         moderate) log_warning "BUILD_COMMIT_DETECTOR_STRICTNESS is set to moderate.." && return 1 ;;
@@ -42,9 +54,14 @@ detect_build_necessity() {
     fi
   done
 
-  echo "build_necessary=true" | tee -a "${GITHUB_ENV:-/dev/null}" "${GITHUB_OUTPUT:-/dev/null}"
+  build_necessary_json_output build true | tee -a "${GITHUB_ENV:-/dev/null}" "${GITHUB_OUTPUT:-/dev/null}"
 }
 
-if (( $# != 2 )); then usage && exit 1; fi
 if "${RUNNER_DEBUG:-false}"; then set -x; fi
-if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then detect_build_necessity "$@"; fi
+
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+  set -euo pipefail 
+
+  if (( $# != 2 )); then usage && exit 1; fi
+   detect_build_necessity "$@";
+fi
